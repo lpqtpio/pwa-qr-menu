69551b0c8904b72185982431f8acb1655f7e0236
JorgeR <jorgerodrigorivera@outlook.com>
2026-03-11 21:54:53 -0500

    Updated file test.js

--- api/test.js
+++ api/test.js
@@ -1,6 +1,8 @@
 export default function handler(req, res) {
   res.status(200).json({ 
     message: "API is working!",
+    env: process.env.NODE_ENV,
+    hasDbString: !!process.env.MONGODB_URI,
     timestamp: new Date().toISOString()
   });
 }
